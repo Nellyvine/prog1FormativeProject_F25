0041fe805a6a5c46256b47d5c01b861c3e5ee918
Nellly <n.mizero@alustudent.com>
2025-12-03 01:30:09 +0400

    Initial Commit

---
+++ .idea/function.py
@@ -0,0 +1,8 @@
+def year ():
+    age = int(input ("How old are you:"))
+    x = 100 - age
+    y = 2025 + x
+    return y
+name = input ("What is your name:")
+print("Hello",name, "you will be a 100 years old in",year())
+
