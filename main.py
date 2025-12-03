bea1d754f44c5b144ad08c39f5fe246500b433c9
Nellly <n.mizero@alustudent.com>
2025-12-04 00:58:08 +0400

    Initial Commit

--- main.py
+++ main.py
@@ -2,5 +2,14 @@ def main():
     bugdet = budgettracker()
 
     while (True):
+        print(' Menu of the Budget Tracker')
+        print('1) Add Income')
+        print('2) Add Expense')
+        print('3) List Transactions')
+        print('4) Filter Transactions')
+        print('5) Show Summary')
+        print('0) Exit')
+
+
 
 
